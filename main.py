9aee653547450dba3a0841a68048e989fc56adcd
Octiupus <prompico@gmail.com>
2025-02-25 11:50:35 +0100

    You spin me right round baby

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 import pygame
 
+player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2, PLAYER_RADIUS=PLAYER_RADIUS)
 
 def main():
     pygame.init()
@@ -11,16 +12,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill((0, 0, 0))
-        Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS).draw(screen) 
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000
         
-
-    
-
-
 if __name__ == "__main__":
     main()
