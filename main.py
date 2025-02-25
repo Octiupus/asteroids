6aa596f2e6134c83079900f17c46b7f9fe4a4470
Octiupus <prompico@gmail.com>
2025-02-25 11:19:54 +0100

    FPS

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill((0, 0, 0))
@@ -12,6 +14,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        
+    
 
 
 if __name__ == "__main__":
