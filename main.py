ba07553867716e50792b6d5cf5db6cd26835d085
Octiupus <prompico@gmail.com>
2025-02-25 11:30:34 +0100

    Added player, circleshape, and player to loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Asteroids
 from constants import *
+from player import Player
 import pygame
 
 
@@ -10,12 +11,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill((0, 0, 0))
+        Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS).draw(screen) 
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000
         
+
     
 
 
