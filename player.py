a34a17ae08936dca4ae346b6271cdee997b5ea8c
Octiupus <prompico@gmail.com>
2025-02-25 12:33:55 +0100

    Added the files I forgot to add

---
+++ player.py
@@ -0,0 +1,44 @@
+from circleshape import *
+from constants import (
+    PLAYER_RADIUS, 
+    PLAYER_SPEED, 
+    PLAYER_TURN_SPEED,
+)
+import pygame
+
+
+class Player(CircleShape): 
+    def __init__(self, x, y, PLAYER_RADIUS):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        super().draw(screen)    
+        pygame.draw.polygon(screen, color=255, points=self.triangle(), width=2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            self.rotation += PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_a]:
+            self.rotation -= PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt   
